7ea6c8a9d571d9fc21cb0a24919a4ced90cd7b5b
David Sheffield <jeremiah.sheffield@gmail.com>
2016-04-05 22:38:42 -0400

    cleaning up for s3 testing

--- app/app.config.js
+++ app/app.config.js
@@ -23,21 +23,6 @@
         controller: 'ResetPassController',
         controllerAs: 'vm'
       }).
-      when('/chris-test', {
-        templateUrl: 'app/test/chris-test.html',
-        controller: 'ChrisTestController',
-        controllerAs: 'vm'
-      }).
-      when('/david-test', {
-        templateUrl: 'app/test/david-test.html',
-        controller: 'DavidTestController',
-        controllerAs: 'vm'
-      }).
-      when('/emmanuel-test', {
-        templateUrl: 'app/test/emmanuel-test.html',
-        controller: 'EmmanuelTestController',
-        controllerAs: 'vm'
-      }).
       otherwise({
         redirectTo: '/sign-in'
       });
